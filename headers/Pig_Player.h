766d79e58f03ef64a87cd43a5533ff90bb0183bf
Jamal Davis <Jamal.Ra.Davis@gmail.com>
2020-08-17 19:02:49 -0700

    Initial commit: Have pig being controlled in world with basic collision detection

---
+++ headers/Pig_Player.h
@@ -0,0 +1,65 @@
+#pragma once
+#include <SDL2/SDL.h>
+#include <list>
+#include "../headers/MapObject.h"
+#include "../headers/AudioPlayer.h"
+
+
+class Pig_Player : public MapObject
+{
+	private:
+		bool dead;
+		bool onGround;
+		bool visible;
+		bool jumpInit;	
+		bool canDoubleJump;
+		bool landing;
+
+		int cOffset_x;
+		int cOffset_y;
+
+		bool canDash;
+		bool dashStart;
+		bool dash_end;
+		bool dashing;
+		double dashStartX;
+		double dashStartY;
+		static constexpr double dashSpeed=15.0;
+		static const int DASH_LEN = 200;
+
+		static const int DRIFT_VAL=5;
+	
+		//Animations
+		int numAnimations;
+		SDL_Texture *animationTexture;
+		SDL_Rect **sprite_rects;
+		int *frameNumbers;
+
+		//Audio
+		AudioPlayer **player_sounds;
+	
+		void getNextPosition();
+		void initAnimation();
+
+	public:
+		enum Player_Actions {STANDING, WALKING, JUMP_START, JUMP_PEAK, FALLING, LANDING, BRAKING_F, BRAKING_R, DASH_START, DASHING, DASH_STOP, SLIDE, ANM_NUM};
+		enum Player_Sounds {JUMP_SFX, SFX_NUM};
+
+		Pig_Player(TileMap *tm, SDL_Renderer *renderTarget_);
+        ~Pig_Player();
+
+		bool isDead() {return dead;}
+		void setDead(bool b) {dead = b;}
+		bool isOnGround() {return onGround;}
+		bool getVisible() {return visible;}
+		void setVisible(bool b) {visible = b;}
+		void dashAction();
+		void dashStop();
+		void setJumping(bool b);		
+		void recharge(){canDash = true; canDoubleJump = true;}
+		SDL_Rect getCollisionRect();
+
+		void reset(double x_, double y_);
+        void update();
+        void draw();
+};
---
+++ src/GameStateManager.cpp
@@ -0,0 +1,107 @@
+#include <stdio.h>
+#include "../headers/GameStateManager.h"
+
+
+GameStateManager::GameStateManager(SDL_Renderer *renderTarget_)
+{
+	renderTarget = renderTarget_;
+	//currentState = MENU_STATE;
+	currentState = PIG_STATE;
+
+	//currentState = WIN_STATE;
+	score = 0;
+	gameStates = new GameState*[NUM_STATES];
+	for (int i=0; i<NUM_STATES; i++)
+	{
+		gameStates[i] = NULL;
+	}
+	loadState(currentState);
+	gameStates[currentState]->init();
+	//setState(currentState);
+	printf("Loaded title state\n");
+	//gameStates[0] = new MenuState(this, renderTarget);
+	//gameStates[1] = new Level1State(this, renderTarget);
+}
+
+GameStateManager::~GameStateManager()
+{
+	for (int i=0; i<NUM_STATES; i++)
+    {
+        if (gameStates[i])
+			delete gameStates[i];
+    }
+	if (gameStates)
+		delete[] gameStates;
+}
+
+void GameStateManager::setState(int state)
+{
+	unloadState(currentState);
+	currentState = state;
+	loadState(currentState);
+	gameStates[currentState]->init();
+}
+void GameStateManager::update()
+{
+	gameStates[currentState]->update();
+}
+void GameStateManager::draw()
+{
+	gameStates[currentState]->draw();
+}
+void GameStateManager::keyPressed(int k)
+{
+	gameStates[currentState]->keyPressed(k);
+}
+void GameStateManager::keyReleased(int k)
+{
+	gameStates[currentState]->keyReleased(k);
+}
+
+void GameStateManager::loadState(int state)
+{
+    if (state >= NUM_STATES || state < 0)
+        return;
+    if (gameStates[state])
+        return;
+
+	switch (state)
+	{
+		case MENU_STATE:
+		{
+			gameStates[state] = new MenuState(this, renderTarget);
+			break;
+		}
+		case TITLE_STATE:
+        {
+            gameStates[state] = new TitleState(this, renderTarget);
+            break;
+        }
+		case LEVEL1_STATE:
+		{
+			gameStates[state] = new Level1State(this, renderTarget);
+			break;
+		}
+		case WIN_STATE:
+		{
+			gameStates[state] =  new WinState(this, renderTarget);
+			break;
+		}
+        case PIG_STATE:
+        {
+            gameStates[state] =  new PigState(this, renderTarget);        
+        }
+        default:
+        {
+
+        }
+	}
+}
+void GameStateManager::unloadState(int state)
+{
+	if (gameStates[state])
+	{
+		delete gameStates[state];
+		gameStates[state] = NULL;
+	}
+}
